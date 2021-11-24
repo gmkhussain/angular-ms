387cb1e6870d6c18e7fba75f14ab39f6b1151a1f
Developer <dev@xyz.com>
2021-11-24 13:55:47 +0500

    bootstrap

--- ng-app/src/app/app-routing.module.ts
+++ ng-app/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './views/frontend/home/home.component';
+
+import { HomeComponent } from './views/frontend/pages/home/home.component';
+import { PostsComponent } from './views/frontend/pages/posts/posts.component';
+import { PostSingleComponent } from './views/frontend/pages/post-single/post-single.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: "post/:id", component: PostSingleComponent }
 ];
 
 @NgModule({
--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './views/frontend/home/home.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { HomeComponent } from './views/frontend/pages/home/home.component';
+import { HeaderComponent } from './views/frontend/layouts/header/header.component';
+import { PostsComponent } from './views/frontend/pages/posts/posts.component';
+import { PostSingleComponent } from './views/frontend/pages/post-single/post-single.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    PostsComponent,
+    PostSingleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
