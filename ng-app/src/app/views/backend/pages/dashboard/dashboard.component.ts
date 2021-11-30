aa849c2a11c676c97729f37e7934875a0657f597
Developer <gmk.hussain@gmail.com>
2021-11-30 19:15:51 +0500

    Check Authentication status on Dashboard URL

--- ng-app/src/app/views/backend/pages/dashboard/dashboard.component.ts
+++ ng-app/src/app/views/backend/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router'; //<-- For Redirect
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router //<-- For Redirect
+  ) { }
 
   ngOnInit(): void {
+
+    // If Token doesnt exist...
+    // Redirect to home URL...
+    if( localStorage.getItem("token")?.length === 0 ) {
+      this.router.navigate(['/']);
+      console.log(localStorage.getItem("token"))
+    }
+
   }
+
+  
 
 }
